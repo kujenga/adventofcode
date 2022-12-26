86f970a219f0639f34fb8678c653d8b2231a60fd
Aaron Taylor <git@ataylor.io>
2022-12-26 15:27:23 -0500

    2022/13: part 2 solution
    Builds on part 1 with sorting functions and iterations.

--- 2022/13/main.py
+++ 2022/13/main.py
@@ -3,10 +3,12 @@
 import sys
 import json
 import itertools
+from functools import cmp_to_key
+from pprint import pprint
 
 
 def ordered(left, right):
-    #  print("ordered:", left, right)
+    #  print(f"ordered: left: {left} ; right: {right}", left, right)
     for (l, r) in itertools.zip_longest(left, right):
         #  print("l, r", l, r, type(l), type(r))
         if l is None:
@@ -28,13 +30,24 @@ def ordered(left, right):
         # Handle true ordering, ignore None.
         if o is True or o is False:
             return o
-
     return None
+
+
+def cmp(left, right):
+    o = ordered(left, right)
+    if o is None:
+        return 0
+    if o is True:
+        return -1
+    if o is False:
+        return 1
+    assert False, f"unknown ordering: {o}"
 
 
 index = 1
 result = 0
 prev = None
+packets = []
 
 for line in sys.stdin:
     line = line.strip()
@@ -45,6 +58,7 @@ for line in sys.stdin:
         continue
 
     data = json.loads(line)
+    packets.append(data)
     # When prev is none, this is the first line in a pair.
     if prev is None:
         prev = data
@@ -57,3 +71,37 @@ for line in sys.stdin:
         result += index
 
 print('sum of ordered indexes:', result)
+
+# Order and find dividers
+
+# Add dividers
+packets.append([[2]])
+packets.append([[6]])
+
+print('ordering packets:')
+pprint(packets)
+
+# sort packets
+packets.sort(key=cmp_to_key(cmp))
+
+print('ordered packets:')
+pprint(packets)
+
+# look for dividers
+div1 = None
+div2 = None
+for (i, pkt) in enumerate(packets):
+    if type(pkt) != list:
+        continue
+    if len(pkt) != 1:
+        continue
+    if type(pkt[0]) != list:
+        continue
+    if len(pkt[0]) != 1:
+        continue
+    if pkt[0][0] == 2:
+        div1 = i + 1
+    if pkt[0][0] == 6:
+        div2 = i + 1
+
+print('divider mul:', div1 * div2)
